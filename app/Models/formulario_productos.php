def98807b121660339c0da99d4d11073c69ec53a
NayeliSilva2322 <146583880+NayeliSilva2322@users.noreply.github.com>
2025-05-22 15:36:07 -0500

    Update formulario_productos.php

--- app/Models/formulario_productos.php
+++ app/Models/formulario_productos.php
@@ -10,7 +10,7 @@ if ($conexion->connect_error) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['id_producto'])) {
-    $stmt = $conexion->prepare("INSERT INTO productos (id_producto, nombre, descripcion, precio_compra, precio_venta, stock, categoria, subcategoria, presentacion, fecha_vencimiento, fabricante, peso_volumen, unidad_medida, ubicacion_almacen, notas)
+    $stmt = $conexion->prepare("INSERT INTO productos (id_producto, nombre, descripcion, precio_compra, precio_venta, stock, categoria, subcategoria,marca, presentacion, fecha_vencimiento, fabricante, peso_volumen, unidad_medida, ubicacion_almacen, notas)
     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
     $stmt->bind_param("sssddisssssssss",
@@ -22,12 +22,11 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['id_producto'])) {
         $_GET['stock'],
         $_GET['categoria'],
         $_GET['subcategoria'],
+        $_GET['marca'],
         $_GET['presentacion'],
         $_GET['fecha_vencimiento'],
         $_GET['fabricante'],
-        $_GET['peso_volumen'],
         $_GET['unidad_medida'],
-        $_GET['ubicacion_almacen'],
         $_GET['notas']
     );
 
